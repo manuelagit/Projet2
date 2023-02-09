8e51f30518af04de8cfea94c2571cd9bd874ce3f
Manuela Raimbault <mr.raimbault@gmail.com>
2023-02-09 15:37:39 +0100

    taille cases

---
+++ Projet2/ViewModels/CreateClubViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Projet2.Models;
+using System.Collections.Generic;
+
+namespace Projet2.ViewModels
+{
+    public class CreateClubViewModel
+    {
+        public Club club { get; set; }
+        public List<Club> Clubs { get; set; }
+
+        public Utilisateur utilisateur { get; set; }
+        public List<Utilisateur> Utilisateurs { get; set; }
+    }
+}
