ecb6d51ed96444d9331a0b19e172f178f2ffaf38
PAUBEL <paul.paubel93@gmail.com>
2023-02-10 16:55:29 +0100

    add CreateEvenementClub view

--- Projet2/Models/BddContext.cs
+++ Projet2/Models/BddContext.cs
@@ -22,7 +22,7 @@ namespace Projet2.Models
 
         public DbSet<SortieAdherent> SortieAdherents { get; set; }
 
-        public DbSet<Adherent> Adherents { get; set; }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
